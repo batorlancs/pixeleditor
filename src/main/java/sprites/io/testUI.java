89ea7bcfebc3ef246b4220c9efa903aa871b45ba
Owen Purdie <purdie@lancaster.ac.uk>
2022-12-07 11:52:23 +0000

    A basic UI and a test that commit works

---
+++ src/main/java/sprites/io/testUI.java
@@ -0,0 +1,56 @@
+package sprites.io;
+
+import java.awt.*;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.Border;
+import java.awt.event.*;
+
+public class testUI
+{
+    JFrame mainFrame = new JFrame();
+
+    JPanel canvasPanel = new JPanel();
+    JPanel toolPanel = new JPanel();
+    JPanel menuPanel = new JPanel();
+    JPanel layerPanel = new JPanel();
+   
+    JButton open = new JButton();
+   
+    public void CreateDisplay()
+    {
+        mainFrame.setSize(1024,640);
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainFrame.setVisible(true);
+        mainFrame.setLayout(null);
+
+
+        //mainPanel.setSize(512,512);
+
+        menuPanel.setBackground(Color.GREEN);
+        menuPanel.setBounds(0,0,1024,64);
+        
+        menuPanel.add(open);
+
+        canvasPanel.setBackground(Color.LIGHT_GRAY);
+        canvasPanel.setBounds(128,64,640,512);
+
+        toolPanel.setBackground(Color.red);
+        toolPanel.setBounds(0,64,128,512);
+
+        layerPanel.setBackground(Color.blue);
+        layerPanel.setBounds(640,64,384,512);
+
+        mainFrame.add(canvasPanel);
+        mainFrame.add(toolPanel);
+        mainFrame.add(layerPanel);
+        mainFrame.add(menuPanel);
+    }
+
+
+
+
+
+}
